649b19ff63efaaea0e706d3bef1e6cf3e89ecc13
Baofeng Xue <xbaofeng@gmail.com>
2015-12-09 13:52:14 -0800

    r

---
+++ model/stock.go
@@ -0,0 +1,8 @@
+package model
+
+type Stock struct {
+	Id             string
+	Name           string
+	Industry       string
+	Niucha_percent int
+}
--- server.go
+++ server.go
@@ -2,41 +2,46 @@ package main
 
 import (
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"github.com/martini-contrib/gzip"
 	"database/sql"
 	_ "github.com/lib/pq"
 	"log"
-	"fmt"
+	"./model"
 )
 
 func main() {
 	m := martini.Classic()
+	m.Use(gzip.All())
+	m.Use(martini.Static("public"))
 
-	m.Get("/", func() string {
+	m.Use(render.Renderer(render.Options{
+		Extensions: []string{".html"}, // Specify extensions to load for templates.
+		Charset: "UTF-8", // Sets encoding for json and html content-types. Default is "UTF-8".
+	}))
+
+	m.Get("/", func(r render.Render) {
 		db, err := sql.Open("postgres", "postgres://postgres:123456@localhost/stock?sslmode=disable")
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		rows, err := db.Query(`select s.id, b.name,  b.industry,ths_percent  from stock s join stock_base b on s.id=b.id
-			where icf_level=50 and ths_percent>=90  and c_date=current_date order by b.industry;`)
+			where icf_level=50 and ths_percent>=90  and c_date=current_date order by s.ths_percent desc;`)
 
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		var line string
+		arr := []model.Stock{}
 		for rows.Next() {
-			var id string
-			var name string
-			var industry string
-			var ths_percent string
-			err = rows.Scan(&id, &name, &industry, &ths_percent)
-			s := fmt.Sprintf("%s\t%s\t%s\t%s\n", id, name, industry, ths_percent)
-			fmt.Println(s)
-			line += s
-
+			s := model.Stock{}
+			err = rows.Scan(&s.Id, &s.Name, &s.Industry, &s.Niucha_percent)
+			arr = append(arr, s)
 		}
-		return line
+		r.HTML(200, "index", arr)
 	})
-	m.RunOnAddr(":80")
+
+
+	m.RunOnAddr(":8888")
 }
